6b907abb502dff8ebb6e82912228947ebc455247
soerengebbert <soerengebbert@23da3d23-e2f9-862c-be8f-f61c6c06f202>
2010-11-10 11:13:22 +0000

    New file and directory layout

---
+++ create_pywps_processes.sh
@@ -0,0 +1,7 @@
+python GrassXMLtoPyWPS.py -x r.univar.xml -p r_univar.py
+python GrassXMLtoPyWPS.py -x r.univar.xml -p r_univar.py
+python GrassXMLtoPyWPS.py -x r.math.xml -p r_math.py
+python GrassXMLtoPyWPS.py -x r.watershed.xml -p r_watershed.py
+python GrassXMLtoPyWPS.py -x v.buffer.xml -p v_buffer.py
+python GrassXMLtoPyWPS.py -x v.to.rast.xml -p v_to_rast.py
+python GrassXMLtoPyWPS.py -x v.voronoi.xml -p v_voronoi.py
